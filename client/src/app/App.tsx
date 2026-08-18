b6c20fea99c81dca5e5d3e934ec0143df94291bb
beebek1 <beebek990@gmail.com>
2026-08-18 23:39:09 +0545

    feat: add app entry point and root component with React Router

---
+++ client/src/app/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from '../shared/components/Navbar'
+import '../styles/App.css'
+import { Toaster } from 'react-hot-toast'
+import ScrollToTop from '../shared/components/ScrollToTop'
+import AppRoutes from './AppRoutes'
+import Footer from '../shared/components/Footer'
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <ScrollToTop />
+      <Toaster position="top-right" />
+      <div>
+        <AppRoutes/>
+      </div>
+      <Footer/>
+    </BrowserRouter>
+  );
+};
+export default App
