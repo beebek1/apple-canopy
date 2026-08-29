e7f7c3ae38e1ae40179f38cc142566981b07b32e
beebek1 <beebek990@gmail.com>
2026-08-29 23:48:56 +0545

    feat(server): wire post and media modules into main router

--- server/src/modules/main.router.ts
+++ server/src/modules/main.router.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import authRoute from "./auth/auth.route.js";
+import  mediaRoute from "./media/media.routes.js";
+import postRoute from "./post/post.routes.js"
 
 
 const router = Router();
 
 router.use("/auth", authRoute);
-router.use("/blog", authRoute);
+router.use("/posts", postRoute);
+router.use("/media", mediaRoute);
 
 export default router;
