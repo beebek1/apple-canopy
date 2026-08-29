306131c893476663f19085b47d1a80b334830c12
beebek1 <beebek990@gmail.com>
2026-08-29 23:48:31 +0545

    feat(server): enable cookie parsing and serve uploads from repository root

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
 
 import rootRouter from "./modules/main.router.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
@@ -21,14 +22,15 @@ app.use(
   }),
 );
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use(cookieParser());
 
 // Routes
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to AppleCanopy API" });
 });
 
-app.use("/api", rootRouter)
+app.use("/api", rootRouter);
 
 // Error Handler
 app.use(errorHandler);
