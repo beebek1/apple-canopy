4698110ba2b71d5eec624e592683b0c2c58ba5a0
beebek1 <beebek990@gmail.com>
2026-08-30 23:07:06 +0545

    added samesite:none

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -29,7 +29,7 @@ export const loginUser = asyncHandler(
     res.cookie("accessToken", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
