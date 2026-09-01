6ae4b21b7a8b21d3917be34787fb085018e7535d
beebek1 <beebek990@gmail.com>
2026-09-02 00:31:40 +0545

    feat(donation): add payment api client

---
+++ client/src/modules/donation/donation.api.ts
@@ -0,0 +1,29 @@
+import apiClient from "../../shared/apiClient";
+import type { CreateCheckoutSessionInput, Donation } from "./doantion.types";
+
+export const createCheckoutSessionApi = async (
+  data: CreateCheckoutSessionInput,
+) => {
+  const res = await apiClient.post(
+    `${import.meta.env.VITE_API_BASE_URL}/api/payment/create-checkout-session`,
+    data,
+  );
+
+  return res.data.data.url as string;
+};
+
+export const getSessionApi = async (sessionId: string): Promise<Donation> => {
+  const res = await apiClient.get(
+    `${import.meta.env.VITE_API_BASE_URL}/api/payment/session/${sessionId}`,
+  );
+
+  return res.data.data;
+};
+
+export const getDonationsApi = async (): Promise<Donation[]> => {
+  const res = await apiClient.get(
+    `${import.meta.env.VITE_API_BASE_URL}/api/payment/donations`,
+  );
+
+  return res.data.data;
+};
