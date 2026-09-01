159ef74acdd3a0657c1c04e622a77d52547b6cc5
beebek1 <beebek990@gmail.com>
2026-09-01 17:38:06 +0545

    feat(session): add client session api calls

--- client/src/modules/admin/auth.api.ts
+++ client/src/modules/admin/auth.api.ts
@@ -4,7 +4,10 @@ import type {
   ApiResponse,
   PostRequest,
   PostListResponse,
+  SessionListResponse
 } from "./auth.types";
+
+// =====================================================AUTHs================================================================
 
 export const loginApi = (data: LoginRequest) =>
   apiClient.post<ApiResponse>("/api/auth/login", data);
@@ -17,6 +20,24 @@ export const resetPasswordApi = (token: string) =>
 
 export const getCurrentUserApi = () =>
   apiClient.get<ApiResponse>(`/api/auth/me`);
+
+
+// =====================================================SESSION================================================================
+export const getSessionsApi = () =>
+  apiClient.get<SessionListResponse>("/api/sessions");
+
+export const revokeSessionApi = (sessionId: string) =>
+  apiClient.delete<ApiResponse>(`/api/sessions/${sessionId}`);
+
+export const revokeOtherSessionsApi = () =>
+  apiClient.delete<ApiResponse>("/api/sessions");
+
+export const logoutApi = () =>
+  apiClient.post<ApiResponse>("/api/sessions/logout");
+ 
+
+
+// =====================================================MEDIA================================================================
 
 export const uploadImageApi = (formData: FormData) =>
   apiClient.post<ApiResponse<{ path: string }>>(`/api/media/upload`, formData);
