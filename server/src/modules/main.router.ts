5c3f122e491e320c856518c4fdcb9242de99e75a
beebek1 <beebek990@gmail.com>
2026-09-01 17:37:49 +0545

    feat(router): register session routes

--- server/src/modules/main.router.ts
+++ server/src/modules/main.router.ts
@@ -4,6 +4,7 @@ import  mediaRoute from "./media/media.routes.js";
 import postRoute from "./post/post.routes.js"
 import commentRoute from "./comment/comment.routes.js"
 import statusRoute from "./status/status.routes.js"
+import sessionRoute from "./session/session.routes.js"
 
 
 const router = Router();
@@ -13,5 +14,6 @@ router.use("/posts", postRoute);
 router.use("/media", mediaRoute);
 router.use("/comments", commentRoute);
 router.use("/status", statusRoute);
+router.use("/sessions", sessionRoute);
 
 export default router;
